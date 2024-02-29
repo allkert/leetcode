ffa3cc6d53e8dbbb5177cb4368d6aef78b647d74
dell_wm <1352736879@qq.com>
2024-02-29 18:25:47 +0800

    afd

--- cpp/code/hashtable.cpp
+++ cpp/code/hashtable.cpp
@@ -3,6 +3,9 @@
 #include<math.h>
 #include<vector>
 #include<unordered_set>
+#include <map>
+#include <unordered_map>
+#include<algorithm>
 
 using namespace std;
 
@@ -60,5 +63,95 @@ public:
             }
             n = getsum(n);
         }    
+    }
+
+
+    // 1. 两数之和
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> a;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            auto iter = a.find(target - nums[i]);
+            if(iter != a.end()){
+                return {iter->second, i};
+            }
+            a.insert(pair<int, int>(nums[i], i));
+        }
+        return {};
+    }
+
+    // 454 四数相加Ⅱ
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D ){
+        unordered_map<int, int> umap;
+        for(int a:A){
+            for(int b: B){
+                umap[a+b]++;
+            }
+        }
+        int res = 0;
+        for(int c: C){
+            for(int d:D){
+                if(umap.find(-(c+d)) != umap.end()){
+                    res += umap[-(c+d)];
+                }
+            }
+        }
+        return res;
+    }
+    
+    // 383 赎金信
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> magazine_counter;
+        for(char s: magazine){
+            magazine_counter[s]++;
+        }
+        for(char s: ransomNote){
+            unordered_map<char, int>::iterator it = magazine_counter.find(s);
+            if(it == magazine_counter.end()){
+                return false;
+            }
+            else{
+                if(it->second == 0){
+                    return false;
+                }
+                else{
+                    it->second--;
+                }
+            }
+        }
+        return true;
+    }
+
+    // 15 三数之和
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for(int i = 0; i < n && nums[i] <= 0; i++){
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int left = i + 1, right = n - 1;
+            while(left < right){
+                if(nums[i]+nums[left]+nums[right] > 0){
+                    right--;
+                }
+                else if(nums[i]+nums[left]+nums[right] < 0){
+                    left++;
+                }
+                else{
+                    res.push_back(vector<int>{nums[i], nums[left], nums[right]});
+                    while(left < right && nums[right]==nums[right-1]){
+                        right--;
+                    }
+                    while(left < right && nums[left] == nums[left+1]){
+                        left++;
+                    }
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return res;
     }
 };
