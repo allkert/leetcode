e747306e0612db0f8438f287f9d4a484ed87bb04
allkert <1362736879@qq.com>
2024-03-11 13:52:49 +0800

    af

--- cpp/code/backtracking.cpp
+++ cpp/code/backtracking.cpp
@@ -190,12 +190,69 @@ public:
     }
 };
 
-// 参考
+// 参考 想想为什么参考答案的效率这么高
 class Solution_39_ref{
-    vector<int> res;
-    void backtracking(string& s, int startIndex, int)
+private:
+    vector<string> res;
+    bool isVaild(const string& s, int start, int end){
+        if(start > end) return false;
+        if(s[start] == '0' && start != end) return false;
+        int num = 0;
+        for(int i = start; i <= end; i++){
+            if(s[i] > '9' || s[i] < '0') return false;
+            num = num * 10 + (s[i] - '0');
+            if(num > 255) return false;
+        }
+        return true;
+    }
+    void backtracking(string& s, int startIndex, int pointNum){
+        if(pointNum == 3){
+            if(isVaild(s, startIndex, s.size()-1)){
+                res.push_back(s);
+            }
+            return;
+        }
+        for(int i = startIndex; i < s.size(); i++){
+            if(isVaild(s, startIndex, i)){
+                s.insert(s.begin()+i+1, '.');
+                backtracking(s, i+2, pointNum+1);
+                s.erase(s.begin()+i+1);
+            }
+            else break;
+        }
+    }
+public:
+    vector<string> restoreIpAddresses(string s) {
+        res.clear();
+        if (s.size() < 4 || s.size() > 12) return res; // 算是剪枝了
+        backtracking(s, 0, 0);
+        return res;
+    }
+
 };
 
+
+class Solution_78 {
+private:
+    vector<vector<int>> res;
+    vector<int> path;
+public:
+    void backtracking(int startIndex, vector<int>& nums){
+        res.push_back(path);
+        if(startIndex == nums.size()){
+            return;
+        }
+        for(int i = startIndex; i < nums.size(); i++){
+            path.push_back(nums[i]);
+            backtracking(i+1, nums);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        backtracking(0, nums);
+        return res;
+    }
+};
 
 int main(){
     return 0;
