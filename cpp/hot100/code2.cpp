48aff3a09f0ac0a830dd984b4dc0d3950d1e2f9d
allkert <1362736879@qq.com>
2024-03-26 13:48:27 +0800

    af

--- cpp/hot100/code2.cpp
+++ cpp/hot100/code2.cpp
@@ -376,9 +376,27 @@ public:
     }
 };
 
-class Solution152 {
+// 42. 接雨水
+class Solution_42{
 public:
-
+    int trap(vector<int>& height) {
+        stack<int> st;
+        st.push(0);
+        int sum = 0;
+        for (int i = 1; i < height.size(); i++) {
+            while (!st.empty() && height[i] > height[st.top()]) {
+                int mid = st.top();
+                st.pop();
+                if (!st.empty()) {
+                    int h = min(height[st.top()], height[i]) - height[mid];
+                    int w = i - st.top() - 1;
+                    sum += h * w;
+                }
+            }
+            st.push(i);
+        }
+        return sum;
+    }
 };
 
 
