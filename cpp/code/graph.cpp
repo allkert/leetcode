2946f372d693b8aaaf4522429ab9366375928e8b
dell_wm <1352736879@qq.com>
2024-04-22 13:08:35 +0800

    afs

---
+++ cpp/code/graph.cpp
@@ -0,0 +1,17 @@
+# include<vector>
+
+using namespace std;
+
+
+// 797. 所有可能的路径
+class Solution_797{
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph){
+
+    }
+
+private:
+    void dfs(vector<vector<int>>& graph, vector<int> path, int cur){
+
+    }
+};
