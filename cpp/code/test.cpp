4e11ba169582a983c38621dc56b7df0c31a352ee
allkert <1362736879@qq.com>
2024-05-14 10:40:19 +0800

    a

--- cpp/code/test.cpp
+++ cpp/code/test.cpp
@@ -1,13 +1,51 @@
 #include<iostream>
-#include<algorithm>
-#include<vector>
-#include<cstring>
-#include <array>
+#include <vector>
+
 
 using namespace std;
 
+// int main() {
+//     // int n;
+//     // cin >> n;
+//     // for(int i = 0; i < n; i++){
+//     //     int zoneNum, leastCover;
+//     //     cin >> zoneNum >> leastCover;
+//     //     vector<int> zone(zoneNum, 0);
+//     //     for(int j = 0; j < zoneNum; j++){
+//     //         cin >> zone[j];
+//     //     }
+//     int zoneNum = 8, leastCover = 3;
+//     vector<int> zone = {4, 5, 6, 7, 9, 9, 6, 8};
+//     int left = 0, right = 0;
+//     int sum = 0;
+//     double res = 0;
+//     while(right < zoneNum){
+//         if(right - left < leastCover){
+//             sum += zone[right];
+//             right++; 
+//         }
+//         else{
+//             double average = sum * 1.0 / (right - left);
+//             if(zone[right] > average){
+//                 sum += zone[right];
+//                 right++;
+//             }
+//             while(right - left >= leastCover){
+//                 res = max(res, sum * 1.0 / (right - left));
+//                 sum -= zone[left];
+//                 left++;
+//             }
+//         }
+//     }
+//     printf("%.3f\n", res);
+// }
+int count(string s){
+    int f = 0;
+    for(int i = 0; i < s.size() - 1; i++){
+        f += 10*(s[i] - '0') + (s[i+1] - '0'); 
+    }
+    return f;
+}
 int main(){
-    array<char, 5> a = {1, 2, 3, 4, 5};
-    char b[5];
-    cin.get(b, 5);
+    cout << count("10101") << endl;
 }
