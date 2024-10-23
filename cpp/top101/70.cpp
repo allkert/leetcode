8cff7e552c0af96acdd52a9be8f8a6a84eb860e2
allkert <1362736879@qq.com>
2024-10-23 23:50:50 +0800

    current_obs

--- cpp/top101/70.cpp
+++ cpp/top101/70.cpp
@@ -1,33 +1,17 @@
-// #include <iostream>
-// #include <vector>
-// using namespace std;
-
-// int main() {
-//     int n;
-//     cin >> n;
-//     vector<int> dp(n + 1, 0);
-//     dp[0] = 1;
-//     for(int i = 1; i <= n; ++i){
-//         for(int j = 0; j < i; ++j){
-//             dp[i] += dp[j];
-//         }
-//     }
-//     cout << dp[n] << endl;
-// }
-// // 64 位输出请用 printf("%lld")
-
-#include<bits/stdtr1c++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
+
 int main() {
-	int n;
-	while (cin >> n) {
-		vector<int> v(n + 1);
-		iota(v.begin(), v.begin() + n + 1, 0);
-		while (v.size() != 1) {
-			for (int j = 0; j < int(v.size()); j++)
-				v.erase(v.begin() + j);
-		}
-		cout << v[0] << endl;
-	}
-	return 0;
+    int n;
+    cin >> n;
+    vector<int> dp(n + 1, 0);
+    dp[0] = 1;
+    for(int i = 1; i <= n; ++i){
+        for(int j = 0; j < i; ++j){
+            dp[i] += dp[j];
+        }
+    }
+    cout << dp[n] << endl;
 }
+// 64 位输出请用 printf("%lld")
